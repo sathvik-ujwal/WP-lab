b68a9956ecc8025410bd18b317e49be55459252d
sathvik-ujwal <sathvikmu04@gmail.com>
2025-01-13 17:20:17 +0530

    week1 assignment

---
+++ week1/form.js
@@ -0,0 +1,10 @@
+const ValidateAge = (age) => {
+  //let age = document.getElementById("age").innerHTML;
+  console.log(age);
+  console.log("JJJj");
+  if (18 < age < 60) {
+    console.log(age);
+  } else {
+    alert("Enter age between 18 and 60");
+  }
+};
